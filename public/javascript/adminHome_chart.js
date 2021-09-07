f5fb9c3d67e481be919cce9853e35b8626491b80
alexkou <alex24@hotmail.gr>
2021-09-07 14:42:41 +0300

    Finish admin map

--- public/javascript/adminHome_chart.js
+++ public/javascript/adminHome_chart.js
@@ -30,8 +30,16 @@ var methodObject = JSON.parse(document.getElementById("methodObject").value)
 for (method in methodObject) {
     chart1.data.labels.push(method)
     chart1.data.datasets[0].data.push(methodObject[method])
+    random_colors(chart1);
 } 
+
+
 
 chart1.update();
 
-console.log(methodObject)
+
+function random_colors(chart) {
+    var color = "rgba(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ",";
+    chart.data.datasets[0].backgroundColor.push(color + "0.6)");
+    chart.data.datasets[0].borderColor.push(color + "1)");
+}
